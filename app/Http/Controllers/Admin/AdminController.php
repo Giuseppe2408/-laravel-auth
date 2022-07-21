bd8c4b2e3e47d91463bfe08933cd1eb296db5048
Giuseppe <giuseppe.acito.ga@gmail.com>
2022-07-21 17:25:26 +0200

    risolto

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class HomeController extends Controller
+class AdminController extends Controller
 {
     /**
      * Create a new controller instance.
@@ -23,6 +23,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('app');
+        return view('admin.dashboard');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Auth::routes();
 
-Route::get('/', 'AdminController@index');
+
+
+Route::middleware('auth')
+   ->namespace('Admin')
+   ->name('admin.')
+   ->prefix('admin')
+   ->group(function () {
+        Route::get('/', 'AdminController@index')->name('dashboard');
+        Route::resource('posts', 'PostController');
+   });
